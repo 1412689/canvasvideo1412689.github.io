e8db29d0f8b1319b6e0330fece8eab56c7085da1
Hoàng Thị Bích Vân <hoangthibichvan95@gmail.com>
2018-12-01 13:43:40 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// Install express only: npm i express
+
+var express = require('express');
+var app = express();
+
+// Define the port to run on
+app.set('port', 8080);
+
+app.use(express.static(__dirname));
+
+// Listen for requests
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('Magic happens on port ' + port);
+});
